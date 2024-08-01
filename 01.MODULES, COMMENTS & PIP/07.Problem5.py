9f48cba0ae3b08bff9ce251b2ff20da01559e416
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-08-01 15:05:14 +0530

    labelTheProgramWithComments

---
+++ 01.MODULES, COMMENTS & PIP/07.Problem5.py
@@ -0,0 +1,15 @@
+# Label the program written in problem 4 with comments.  
+
+import os
+
+# select the directory whose contet you want to list.
+
+directory_path = '/'
+
+# use the os module to list the directory content.
+
+content = os.listdir(directory_path)
+
+# print the content ot the directory.
+
+print(content)
