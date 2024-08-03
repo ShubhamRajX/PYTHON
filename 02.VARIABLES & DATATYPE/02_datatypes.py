d82b81e28cd6c6304de9df1a40f1d733f6f3d289
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-08-03 12:46:32 +0530

    dataTypes

---
+++ 02.VARIABLES & DATATYPE/02_datatypes.py
@@ -0,0 +1,15 @@
+a = 5 # a is an integer
+
+b = 7.33 # b is a floating point number
+
+c = "Shubham" # c is a string
+
+d = True # d is a boolean variable
+
+e = None # e is a none type variable
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
