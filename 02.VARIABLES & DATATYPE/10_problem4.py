ddfcfa83060b068ffb46d34aeab1ff438445ad21
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-08-03 22:26:34 +0530

    comparisonBetweenNumbers

---
+++ 02.VARIABLES & DATATYPE/10_problem4.py
@@ -0,0 +1,10 @@
+# Use comparison operator to find out whether ‘a’ given variable a is greater than 
+# ‘b’ or not. Take a = 34 and b = 80
+
+# a = int(input("Enter first number : "))
+# b = int(input("Enter second number : "))
+
+a = 34
+b = 80
+
+print("a is greater than b is :",a>b)
