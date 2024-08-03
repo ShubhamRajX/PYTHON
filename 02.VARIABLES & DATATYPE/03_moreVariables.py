c66a642164d6964957ea3b879c4da490dac0d381
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-08-03 15:47:53 +0530

    differntTypes

---
+++ 02.VARIABLES & DATATYPE/03_moreVariables.py
@@ -0,0 +1,16 @@
+a = 23
+
+aaa = 435
+
+harry = "Shubham"
+
+raj = "Anisha"
+
+_vicky = 34
+
+# @sameer = 56 # Invalid due to @ symbol
+# s@meer # Invalid due to @ symbol
+
+print(a)
+print(harry)
+print(raj)
