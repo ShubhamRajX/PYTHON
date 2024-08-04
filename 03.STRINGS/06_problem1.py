3aced02e4b5371a36e0d918f4760134fa61cc8ad
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-08-04 20:02:32 +0530

    followedByGoodAfternoon

---
+++ 03.STRINGS/06_problem1.py
@@ -0,0 +1,12 @@
+''' Write a python program to display a user entered name followed by Good 
+Afternoon using input () function.'''
+
+name = input("Enter your name: ")
+
+# print(f"Good Morning, {name} ") 
+# print(f"Good Afternoon, {name} ") 
+# print(f"Good Night, {name} ") 
+
+print(f"Good Morning, {name.capitalize()} ") 
+print(f"Good Afternoon, {name.capitalize()} ") 
+print(f"Good Night, {name.capitalize()} ")
