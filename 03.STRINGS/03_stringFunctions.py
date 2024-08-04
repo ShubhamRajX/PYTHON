74dcfdeecfa310c8911d4d91481a29103d891940
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-08-04 19:29:02 +0530

    stringFunction

---
+++ 03.STRINGS/03_stringFunctions.py
@@ -0,0 +1,15 @@
+name = "shubham"
+
+# length() function
+print(len(name))
+
+# sting .endswith()
+print(name.endswith("am"))
+print(name.endswith("raj"))
+
+# sting .startswith()
+print(name.startswith("ha"))
+print(name.startswith("sh"))
+
+# string .capitalize()
+print(name.capitalize())        #only first letter capitalize
