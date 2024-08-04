78ddd0e0c16da67f780d20b23cbf2e7a5b349c40
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-08-04 19:06:21 +0530

    indexing

---
+++ 03.STRINGS/01_intro_to_strings.py
@@ -0,0 +1,17 @@
+name = "Shubham"
+
+nameshort1 = name[0:3]        # start from index 0 all the way till 3 (excluding 3)
+nameshort2 = name[0:5]
+nameshort3 = name[0:7]
+
+print(nameshort1)
+print(nameshort2)
+print(nameshort3)
+
+character1 = name[1]
+character2 = name[3]
+character3 = name[5]
+
+print(character1)
+print(character2)
+print(character3)
