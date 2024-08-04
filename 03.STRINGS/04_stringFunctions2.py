ff3cb0f7acac3995687adc7ebff1f922c3dc1d95
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-08-04 19:40:05 +0530

    moreStringFunction

---
+++ 03.STRINGS/04_stringFunctions2.py
@@ -0,0 +1,15 @@
+raj = "Anisha Das"
+
+# string .count()
+count = raj.count("a")
+print(count)
+
+print(raj.count("s"))
+
+# string .find()
+index = raj.find("Das")             # it returns the index value of first letter of that word
+print(index)
+
+# string .replace()
+replaced_string = raj.replace("Das" , "Raj")
+print(replaced_string)
