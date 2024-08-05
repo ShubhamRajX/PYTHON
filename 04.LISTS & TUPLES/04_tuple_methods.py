9d9a7e28ed542a7058307594cb593ca04859c5d3
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-08-05 22:45:55 +0530

    methodOfTuple

---
+++ 04.LISTS & TUPLES/04_tuple_methods.py
@@ -0,0 +1,13 @@
+a = (1,45,342,3424,False, 45, "Anisha", "Shubham")
+print(a) 
+
+no = a.count(45)
+print(no)
+
+i = a.index(3424)
+print(i)
+
+# n = a.index(77)
+# print(n)              #return error
+
+print(len(a))
