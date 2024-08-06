32abad2a9ff4e61a370929ccb791f1a36eaba9d3
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-08-06 22:30:21 +0530

    setsMethods

---
+++ 05.DICTIONARY & SETS/04_set_methods.py
@@ -0,0 +1,17 @@
+s = {1, 5, 32, 54,5, 5, 5, "Shubham"}
+
+print(s, type(s))
+
+s.add(566)
+print(s, type(s))
+
+s.remove(1)
+print(s, type(s))
+
+s.pop()
+print(s)
+
+print(len(s))
+
+s.clear()
+print(s)
