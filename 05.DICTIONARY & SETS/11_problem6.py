c99e05dea471931d656e1f938b8391e50388c66d
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-08-31 19:26:19 +0530

    useEmptyDictionary

--- 05.DICTIONARY & SETS/11_problem6.py
+++ 05.DICTIONARY & SETS/11_problem6.py
@@ -21,3 +21,17 @@ d.update({name: lang})
 
 
 print(d)
+
+
+
+''' If the names of 2 friends are same; what will happen to the program in problem 
+6? '''
+
+# The values entered later will be updated
+
+
+
+''' If languages of two friends are same; what will happen to the program in problem 
+6? '''
+
+# Nothing will happen. The values can be same
