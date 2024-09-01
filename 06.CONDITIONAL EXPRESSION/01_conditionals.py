15a628c56a2b97c0a819b8d81ae3e0dffe3ee0c7
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-09-01 19:16:15 +0530

    ifElse

---
+++ 06.CONDITIONAL EXPRESSION/01_conditionals.py
@@ -0,0 +1,15 @@
+'''Quick Quiz: Write a program to print yes when the age entered by the user is greater 
+than or equal to 18. '''
+
+a = int(input("Enter your age: "))
+
+# If else statement
+if(a>=18):
+    print("You are above the age of consent")
+    print("Good for you")
+
+else:
+    print("You are below the age of consent")
+
+
+print("End of Program")
