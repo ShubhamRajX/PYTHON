fb64877d5282df0cb398d63c18d8c34dadc416b2
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-10-19 12:40:23 +0530

    talkingAboutOrNot

---
+++ 06.CONDITIONAL EXPRESSION/10_problem7.py
@@ -0,0 +1,11 @@
+# Write a program to find out whether a given post is talking about “Harry” or not. 
+
+post = input("Enter the post: ")
+
+
+
+if("shubham" in post.lower()):
+    print("This post is talking about shubham")
+
+else:
+    print("This post is not talking about shubham")
