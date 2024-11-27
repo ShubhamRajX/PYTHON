bd0621d3f521dd2140718fa00d3995cbff6a79ef
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-11-27 23:01:59 +0530

    arguments

---
+++ 08.FUNCTIONS & RECURSIONS/03_function_with_arguments.py
@@ -0,0 +1,9 @@
+def goodDay(name, ending):
+    print("Good Day, " + name)
+    print(ending)
+    return "ok"
+
+# a = goodDay("Harry", "Thank you") 
+# a = goodDay("Shubham", "Good Day") 
+a = goodDay("Anisha", "Good Byee") 
+print(a)
