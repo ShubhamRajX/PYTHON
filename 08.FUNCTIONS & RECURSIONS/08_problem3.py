1a80affbe70175900d5cc50e9c4a178218ea768d
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-11-27 23:17:36 +0530

    addNewLineAtEnd

---
+++ 08.FUNCTIONS & RECURSIONS/08_problem3.py
@@ -0,0 +1,8 @@
+# How do you prevent a python print() function to print a new line at the end. 
+
+# (end = " ") is used to avoid new line.
+
+print("a")
+print("b")
+print("c", end="")
+print("d", end=""), 
